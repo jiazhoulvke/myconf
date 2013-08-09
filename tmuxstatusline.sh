cbf24ff3130c9c5cbee1285c8af0bd30705244b1
jiazhoulvke <jiazhoulvke@gmail.com>
2013-08-10 02:14:20 +0800

    修改tmux配置

--- tmuxstatusline.sh
+++ tmuxstatusline.sh
@@ -4,4 +4,4 @@
 #cpustr=`top -n 1|head -n 3|tail -n 1 | gawk '{printf("%s"),$2}'`
 cpustr=`vmstat|tail -n 1 | awk '{print $13}'`
 memstr=`free | head -n 2 | tail -n 1 | awk '{printf("%.1f",$3/$2*100)}' `
-echo "[CPU:"$cpustr"%][MEM:"$memstr"%]"
+echo "CPU:"$cpustr"% MEM:"$memstr"%"
