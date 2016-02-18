228c8705800c7ee1203205952cbbe75d5676362a
jiazhoulvke <jiazhoulvke@gmail.com>
2016-02-18 11:44:20 +0800

    加入zshrc

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 mv ~/.bashrc ~/.bashrc.bak
+mv ~/.zshrc ~/.zshrc.bak
 mv ~/.Xdefaults ~/.Xdefaults.bak
 mv ~/.Xresources ~/.Xresources.bak
 mv ~/.screenrc ~/.screenrc.bak
 mv ~/.tmux.conf ~/.tmux.conf.bak
 ln -s ~/.myconf/bashrc ~/.bashrc
+ln -s ~/.myconf/zshrc ~/.zshrc
 ln -s ~/.myconf/Xdefaults ~/.Xdefaults
 ln -s ~/.myconf/Xdefaults ~/.Xresources
 ln -s ~/.myconf/urxvt/ ~/.urxvt
